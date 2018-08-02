d68fdfb2daa63c9de047706b1bce3f81beffe9a4
Shoaib Ahmad Gondal <kool.gondal@gmail.com>
2018-08-02 18:56:08 +0300

    initial commit

---
+++ mutawif-backend/src/test/java/com/hajjed/mutawifbackend/MutawifBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hajjed.mutawifbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MutawifBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
